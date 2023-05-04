2e57ca059e037294e0b2abd2878a927277be0f0d
Warmarth <godsonibeaka@gmail.com>
2023-05-04 16:21:55 -0700

    minimum

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+def minOperations(n):
+    """model performes only two functions copy all and paste
+    H = single character
+    n = number of times a character will be pasted{iterate}
+
+    """
+     md = 1
+    copyall = 0
+    paste = 0
+    while md < n:
+        remainder = n - md
+        if (remainder % md == 0):
+            copyall = md
+            md += copyall
+            paste += 2
+        else:
+            md += copyall
+            paste += 1
+    return paste
