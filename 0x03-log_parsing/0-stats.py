76fb5a3707d641482898becf608f074e467e4687
Warmarth <godsonibeaka@gmail.com>
2023-05-12 01:37:02 +0100

    restored

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,45 @@
+#!/usr/bin/python3
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-# store the count of all status codes in a dictionary
-status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0}
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-count = 0  # keep count of the number lines counted
+counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
+for line_number, line in enumerate(sys.stdin, 1):
+    # Split the line into its components
+    parts = line.split()
+    if len(parts) != 10:
+        # If the line format is invalid, skip it
+        continue
 
-        if len(line_list) > 4:
-            status_code = line_list[-2]
-            file_size = int(line_list[-1])
+    ip_address, _, _, date, _, method, path, _, code, size = parts
 
-            # check if the status code receive exists in the dictionary and
-            # increment its count
-            if status_code in status_codes_dict.keys():
-                status_codes_dict[status_code] += 1
+    try:
+        code = str(code)
+        size = int(size)
+    except ValueError:
+        # If the status code or file size is not an integer, skip the line
+        continue
 
-            # update total size
-            total_size += file_size
+    # Update metrics
+    if code in cache.keys():
+        cache[code] += 1
+    total_size += size
 
-            # update count of lines
-            count += 1
-
-        if count == 10:
-            count = 0  # reset count
-            print('File size: {}'.format(total_size))
-
-            # print out status code counts
-            for key, value in sorted(status_codes_dict.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(status_codes_dict.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    # Print metrics every 10 lines
+    counter += 1
+    if counter == 10:
+        counter = 0
+        print('File size: {}'.format(total_size))
+        for key, value in sorted(cache.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
+else:
+ print('File size: {}'.format(total_size))
+ for key, value in sorted(cache.items()):
+    if value != 0:
+       print('{}: {}'.format(key, value))
