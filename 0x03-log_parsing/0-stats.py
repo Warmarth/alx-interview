885f7fbfdf7704bf3207eeacad78636f8db05bf7
Warmarth <godsonibeaka@gmail.com>
2023-05-12 01:09:57 +0100

    fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
+'''a script that reads stdin line by line and computes metrics'''
 
-"""Script that reads stdin line by line and computes metrics"""
 
 import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+for line_number, line in enumerate(sys.stdin, 1):
+    # Split the line into its components
+    parts = line.split()
+    if len(parts) != 10:
+        # If the line format is invalid, skip it
+        continue
 
+    ip_address, _, _, date, _, method, path, _, code, size = parts
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
+    try:
+        code = str(code)
+        size = int(size)
+    except ValueError:
+        # If the status code or file size is not an integer, skip the line
+        continue
 
-count = 0
-size = 0
+    # Update metrics
+    if code in cache.keys():
+        cache[code] += 1
+    total_size += size
 
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    # Print metrics every 10 lines
+    counter += 1
+    if counter == 10:
+        counter = 0
+        print('File size: {}'.format(total_size))
+        for key, value in sorted(cache.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
+    else:
+        print('File size: {}'.format(total_size))
+        for key, value in sorted(cache.items()):
+            if value != 0:
+             print('{}: {}'.format(key, value))
