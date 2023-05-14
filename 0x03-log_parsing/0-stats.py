abeff9407f2a74bc2c1421336213392a821fcfb1
Warmarth <godsonibeaka@gmail.com>
2023-05-14 02:26:23 +0100

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,31 +9,29 @@ cache = {'200': 0, '301': 0, '400': 0, '401': 0,
 total_size = 0
 counter = 0
 
-for line_number, line in enumerate(sys.stdin, 1):
-    # Split the line into its components
-    parts = line.split()
-    if len(parts) != 10:
-        # If the line format is invalid, skip it
-        continue
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-    ip_address, _, _, date, _, method, path, _, code, size = parts
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-    try:
-        code = str(code)
-        size = int(size)
-    except ValueError:
-        # If the status code or file size is not an integer, skip the line
-        continue
+except Exception as err:
+    pass
 
-    # Update metrics
-    if code in cache.keys():
-        cache[code] += 1
-    total_size += size
-
-    # Print metrics every 10 lines
-    counter += 1
-    if counter == 10:
-        counter = 0
-        print('File size: {}'.format(total_size))
-        for key, value in sorted(cache.items()):
-                print('{}: {}'.format(key, value))
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
