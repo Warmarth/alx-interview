6a60b152c70a3d7f47a0a5b69fb1c54d35bc2692
Warmarth <godsonibeaka@gmail.com>
2023-06-22 22:50:00 +0000

    done

--- 0x09-island_perimeter/0-island-perimeter.py
+++ 0x09-island_perimeter/0-island-perimeter.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-"""Check if the grid is empty"""
+"""
+Island Perimeter:
+    returns the perimeter of the island described in grid
+"""
 
 
 def island_perimeter(grid):
-    """Check if the grid is empty"""
-    if not grid:
-        return 0
-
-    # Get the dimensions of the grid
-    rows, cols = len(grid), len(grid[0])
-
-    # Initialize the perimeter to zero
+    """island perimenter function"""
     perimeter = 0
-
-    # Loop over all cells in the grid
-    for i in range(rows):
-        for j in range(cols):
-            # Check if the current cell is land
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                # Check if the cell to the left is water or out of bounds
-                if j == 0 or grid[i][j-1] == 0:
-                    perimeter += 1
-                # Check if the cell to the right is water or out of bounds
-                if j == cols-1 or grid[i][j+1] == 0:
-                    perimeter += 1
-                # Check if the cell above is water or out of bounds
-                if i == 0 or grid[i-1][j] == 0:
-                    perimeter += 1
-                # Check if the cell below is water or out of bounds
-                if i == rows-1 or grid[i+1][j] == 0:
-                    perimeter += 1
-
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
     return perimeter
